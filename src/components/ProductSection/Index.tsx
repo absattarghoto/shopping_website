55a19cdefaafde502c2acd434987b85d25831e26
Abdul Sattar <absattar2053@gmail.com>
2025-03-24 19:22:16 +0500

    solve eroor witch shown in versel

--- src/components/ProductSection/Index.tsx
+++ src/components/ProductSection/Index.tsx
@@ -1,35 +1,32 @@
 const AnimatedCarousel = () => {
-    return (
-      <div className="relative w-full overflow-hidden ">
-         <div>
+  return (
+    <div className="relative w-full overflow-hidden">
+      <div>
         <p className="text-2xl sm:text-3xl font-bold text-[#101913] py-8">
-          Populer Products From
+          Popular Products From
           <br className="hidden sm:block" />
           All Brands
         </p>
       </div>
-        <div className="flex gap-6 w-max animate-[scroll_20s_linear_infinite] mb-20">
-          {[1, 2, 3, 4, 5, 1, 2, 3, 4, 5].map((item, index) => (
-            <div
-              key={index}
-              className="relative h-[400px] w-[300px] flex-shrink-0 overflow-hidden rounded-2xl "
-            >
-              <img
-                src={`Images/Group 2 ${
-                  index > 4 ? `(${index - 5})` : index > 0 ? `(${index})` : ""
-                }.png`}
-                alt={`Carousel item ${index + 1}`}
-                className="h-full w-full object-cover transform transition-transform duration-700 hover:scale-110"
-              />
-            </div>
-          ))}
-        </div>
-  
-        {/* Gradient Overlays */}
-         </div>
-    );
-  };
-  
-  export default AnimatedCarousel;
-  
-     
+
+      <div className="flex gap-6 w-max animate-[scroll_20s_linear_infinite] mb-20">
+        {[1, 2, 3, 4, 5, 1, 2, 3, 4, 5].map((_, index) => (
+          <div
+            key={index}
+            className="relative h-[400px] w-[300px] flex-shrink-0 overflow-hidden rounded-2xl"
+          >
+            <img
+              src={`Images/Group 2 ${
+                index > 4 ? `(${index - 5})` : index > 0 ? `(${index})` : ""
+              }.png`}
+              alt={`Carousel item ${index + 1}`}
+              className="h-full w-full object-cover transform transition-transform duration-700 hover:scale-110"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default AnimatedCarousel;
